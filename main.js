29aee53b26b755620382880dfb62f39a34f804cc
abdul-pste <94234927+abdul-pste@users.noreply.github.com>
2021-11-14 11:18:44 -0500

    Update main.js

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ var ws = document.getElementById('w_seconds');
 var bm = document.getElementById('b_minutes');
 var bs = document.getElementById('b_seconds');
 
+var bull = document.getElementById("bull-image")
+let response = document.getElementById("response").innerHTML
+
 //store a reference to a timer variable
 var startTimer;
 
@@ -17,22 +20,35 @@ start.addEventListener('click', function(){
     } else {
         alert("Timer is already running");
     }
+
+    if (bull.src = "https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-throw-up-bull-emoji-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png"){
+        bull.src = bull.src.replace("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-throw-up-bull-emoji-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png", "https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-grinning-bull-emoji-vitaliy-gorbachev-lineal-color-vitaly-gorbachev-1.png");
+
+    }
+
+    document.getElementById("response").innerHTML = "Keep up the good work!";
 })
 
 reset.addEventListener('click', function(){
-    wm.innerText = 25;
+    document.getElementById("response").innerHTML = "Ok Fine, Lets restart :)."
+    bull.src = "https://icons8.com/icon/8oWNoINt7YLY/devil"
+    wm.innerText = 0;
     ws.innerText = "00";
 
-    bm.innerText = 5;
+    bm.innerText = 0;
     bs.innerText = "00";
 
     document.getElementById('counter').innerText = 0;
     stopInterval()
     startTimer = undefined;
+
 })
 
 stop.addEventListener('click', function(){
+    bull.src = bull.src.replace("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-grinning-bull-emoji-vitaliy-gorbachev-lineal-color-vitaly-gorbachev-1.png", "https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-throw-up-bull-emoji-vitaliy-gorbachev-lineal-color-vitaly-gorbachev.png");
+    document.getElementById("response").innerHTML = "Why'd you stop? Ewww";
     stopInterval()
+    console.log(bull.src)
     startTimer = undefined;
 })
 
@@ -65,7 +81,10 @@ function timer(){
         bm.innerText = 5;
         bs.innerText = "00";
 
-        document.getElementById('counter').innerText++;
+        document.getElementById('points-counter').innerText.value += 1000;
+
+        bull.src.replace("https://img.icons8.com/external-vitaliy-gorbachev-lineal-color-vitaly-gorbachev/60/000000/external-grinning-bull-emoji-vitaliy-gorbachev-lineal-color-vitaly-gorbachev-1.png", "https://icons8.com/icon/yVeWcyC8WBRz/hide");
+        document.getElementById("response").innerHTML = "Congrats!, Take a 5 minute break."
     }
 }
 
@@ -73,3 +92,5 @@ function timer(){
 function stopInterval(){
     clearInterval(startTimer);
 }
+
+
